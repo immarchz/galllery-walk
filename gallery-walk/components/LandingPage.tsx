d41fdc83d57e3290ce23479956a8c99fd6336a27
immarchz <marcharissawat@gmail.com>
2023-07-07 13:27:52 +0700

    Test Commit

---
+++ gallery-walk/components/LandingPage.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>
+        Hello Everybodyyyyyyyyyyyyyy
+      </h1>
+      <p>
+        Test test
+      </p>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ gallery-walk/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <header className='w-full absolute z-10'>
+        <nav className='max-w-[1440px] mx-auto
+        flex justify-between items-center sm:px-16
+        px-6 py-4'>
+        <Link
+        className='flex
+        justify-center items center'
+      href="/">
+        <Image 
+        src="/Logo.svg"
+        alt="Gallery Walk Logo"
+        width={118}
+        height={18}
+        className='object-contain'
+        />
+      </Link>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
