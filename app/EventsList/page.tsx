cdfa852f0c4eae290c97725ff2a568ff058a192e
immarchz <marcharissawat@gmail.com>
2023-07-13 14:55:56 +0700

    Add EventList Page

---
+++ app/EventsList/page.tsx
@@ -0,0 +1,12 @@
+
+import Image from 'next/image'
+
+export default function EventsList() {
+  return (
+    <main className="overflow-hidden">
+      <div className='flex p-4 text-white h-screen bg-black'>
+        Test
+      </div>
+    </main>
+  )
+}
