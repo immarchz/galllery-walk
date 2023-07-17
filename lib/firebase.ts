1b9e011c19f427b92753141fcd279837f0f9c402
immarchz <marcharissawat@gmail.com>
2023-07-17 21:37:28 +0700

    Create firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCpuDcqdAJU5iR-T9Wwk5fYDMHRUIRY_U",
+  authDomain: "gallery-walk-b3312.firebaseapp.com",
+  projectId: "gallery-walk-b3312",
+  storageBucket: "gallery-walk-b3312.appspot.com",
+  messagingSenderId: "212046405718",
+  appId: "1:212046405718:web:816da28c19950ae98e9bf7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
