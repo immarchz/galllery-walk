a543adc3c19070185f8f2c62abfd0da408831964
immarchz <marcharissawat@gmail.com>
2023-09-20 16:36:50 +0700

    Updated

--- app/EventsList/page.tsx
+++ app/EventsList/page.tsx
@@ -3,14 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Divider, Button, Row, Col } from "antd";
 import * as React from "react";
-import { TextField } from "@mui/material";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { useSession } from "next-auth/react";
 
 interface currentEventListInterface {
@@ -66,7 +59,7 @@ const eventArr2: incommingEventListInterface[] = [
     linkTo: "/SingleEvent",
     img: "/xpace.svg",
 
-    eventName: "XSPACE ART HALL V.2 – ENLIVEN",
+    eventName: "XSPACE ART HALL V.2 – 231231232 ",
     time: "Sun, Dec 12, 12:00 PM",
     location: "Xspace Gallery",
   },
@@ -74,7 +67,15 @@ const eventArr2: incommingEventListInterface[] = [
     linkTo: "/SingleEvent",
     img: "/ghibil.svg",
 
-    eventName: "The World of Studio Ghibli’s Animation Central World",
+    eventName: "The World of Studio Ghibli’s",
+    time: "Sat, Dec 23, 7:00 PM",
+    location: "Teayii Arts Work",
+  },
+  {
+    linkTo: "/SingleEvent",
+    img: "/ghibil.svg",
+
+    eventName: "The World of Studio Ghibli’s",
     time: "Sat, Dec 23, 7:00 PM",
     location: "Teayii Arts Work",
   },
@@ -107,123 +108,287 @@ export default function EventsList() {
 
   const { data: session } = useSession();
 
-  const [openModal, setOpenModal] = React.useState(false);
-  const handleOpen = () => setOpenModal(true);
-  const handleClose = () => setOpenModal(false);
-
   if (session && session?.user) {
     return (
-      <div className="text-white">
-        <Row className="mb-10" justify={"center"}>
+      <div className="text-white mx-10">
+        <Row className="mb-10 text-xl" justify={"center"}>
           Current Events
         </Row>
         <Row gutter={[24, 24]} justify={"center"}>
-          {currentEvent.map(
-            (event: currentEventListInterface, index: number) => (
-              <div key={index} className="flex-1">
-                <Row gutter={[32, 32]}>
+          <Row gutter={[24, 24]} justify={"start"}>
+            {currentEvent.map(
+              (event: currentEventListInterface, index: number) => (
+                <>
                   <Col span={12}>
-                    <Link href={event.linkTo}>
-                      <Image src={event.img} alt="" width={291} height={194} />
-                    </Link>
-                  </Col>
-
-                  <Col span={12}>
-                    <Row gutter={[24, 24]}>
-                      <Col span={20}>{event.eventName}</Col>
-                      <Col span={20}>{event.time}</Col>
-                      <Col span={20}>{event.location}</Col>
+                    <Row>
+                      <Col>
+                        <Link href={event.linkTo}>
+                          <Image
+                            src={event.img}
+                            alt=""
+                            width={300}
+                            height={200}
+                          />
+                        </Link>
+                      </Col>
+                      <Col
+                        className="mx-3 text-ellipsis overflow-hidden"
+                        style={{ width: 250 }}
+                      >
+                        <Row gutter={[24, 24]}>
+                          <Col className="text-xl">{event.eventName}</Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                          <Col>{event.time}</Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                          <Col className="mt-5">{event.location}</Col>
+                        </Row>
+                      </Col>
                     </Row>
                   </Col>
-                </Row>
-              </div>
-            )
-          )}
-          {currentEvent.length % 2 === 1 ? (
-            <div className="flex-1"></div>
-          ) : null}
+                </>
+              )
+            )}
+          </Row>
         </Row>
         <Row justify={"center"}>
           <Col span={22}>
             <Divider className=" h-4  border-white" />
           </Col>
         </Row>
+
+        <Row className="mb-10 text-xl" justify={"center"}>
+          Incoming Events
+        </Row>
+        <Row gutter={[24, 24]} justify={"center"}>
+          <Row gutter={[24, 24]} justify={"start"}>
+            {incommingEvent.map(
+              (event: incommingEventListInterface, index: number) => (
+                <>
+                  <Col span={12}>
+                    <Row>
+                      <Col>
+                        <Link href={event.linkTo}>
+                          <Image
+                            src={event.img}
+                            alt=""
+                            width={300}
+                            height={200}
+                          />
+                        </Link>
+                      </Col>
+                      <Col
+                        className="mx-3 text-ellipsis overflow-hidden"
+                        style={{ width: 250 }}
+                      >
+                        <Row gutter={[24, 24]}>
+                          <Col className="text-xl">{event.eventName}</Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                          <Col>{event.time}</Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                          <Col className="mt-5">{event.location}</Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                </>
+              )
+            )}
+          </Row>
+        </Row>
+        <Row justify={"center"}>
+          <Col span={22}>
+            <Divider className=" h-4  border-white" />
+          </Col>
+        </Row>
+
+        <Row className="mb-10 text-xl" justify={"center"}>
+          Completed Events
+        </Row>
+        <Row gutter={[24, 24]} justify={"center"}>
+          <Row gutter={[24, 24]} justify={"start"}>
+            {completeEvent.map(
+              (event: completeEventListInterface, index: number) => (
+                <>
+                  <Col span={12}>
+                    <Row>
+                      <Col>
+                        <Link href={event.linkTo}>
+                          <Image
+                            src={event.img}
+                            alt=""
+                            width={300}
+                            height={200}
+                          />
+                        </Link>
+                      </Col>
+                      <Col
+                        className="mx-3 text-ellipsis overflow-hidden"
+                        style={{ width: 300 }}
+                      >
+                        <Row gutter={[24, 24]}>
+                          <Col className="text-xl">{event.eventName}</Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                          <Col>{event.time}</Col>
+                        </Row>
+                        <Row gutter={[24, 24]}>
+                          <Col className="mt-5">{event.location}</Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </Col>
+                </>
+              )
+            )}
+          </Row>
+        </Row>
+        <Row justify={"center"} className="my-16">
+          <Button className=" bg-white">Create Event</Button>
+        </Row>
       </div>
     );
   }
   return (
-    <div className="flex items-center justify-center min-h-screen bg-cover bg-black text-white">
-      <div className="flex flex-col overflow-hidden ">
-        <h1 className="text-xl"> Current Event </h1>
-        <div className="flex flex-col md:flex-row">
+    <div className="text-white mx-10">
+      <Row className="mb-10 text-xl" justify={"center"}>
+        Current Events
+      </Row>
+      <Row gutter={[24, 24]} justify={"center"}>
+        <Row gutter={[24, 24]} justify={"start"}>
           {currentEvent.map(
             (event: currentEventListInterface, index: number) => (
-              <div
-                key={index}
-                className="w-full md:w-1/2 px-10  text-white mt-5"
-              >
-                <div className="flex flex-wrap  ">
-                  <Link href={event.linkTo}>
-                    <Image src={event.img} alt="" width={291} height={194} />
-                  </Link>
-                  <div className="flex flex-col ml-5">
-                    <h1 className="text-[20px]">{event.eventName}</h1>
-                    <p className="text-[15px]">{event.time}</p>
-
-                    <p>{event.location}</p>
-                  </div>
-                </div>
-              </div>
+              <>
+                <Col span={12}>
+                  <Row>
+                    <Col>
+                      <Link href={event.linkTo}>
+                        <Image
+                          src={event.img}
+                          alt=""
+                          width={300}
+                          height={200}
+                        />
+                      </Link>
+                    </Col>
+                    <Col
+                      className="mx-3 text-ellipsis overflow-hidden"
+                      style={{ width: 250 }}
+                    >
+                      <Row gutter={[24, 24]}>
+                        <Col className="text-xl">{event.eventName}</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col>{event.time}</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col className="mt-5">{event.location}</Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Col>
+              </>
             )
           )}
-        </div>
+        </Row>
+      </Row>
+      <Row justify={"center"}>
+        <Col span={22}>
+          <Divider className=" h-4  border-white" />
+        </Col>
+      </Row>
 
-        <Divider className="my-4 h-4  border-white" variant="middle" />
-
-        <h1 className="text-xl">Incomming</h1>
-        <div className="flex flex-col md:flex-row">
+      <Row className="mb-10 text-xl" justify={"center"}>
+        Incoming Events
+      </Row>
+      <Row gutter={[24, 24]} justify={"center"}>
+        <Row gutter={[24, 24]} justify={"start"}>
           {incommingEvent.map(
             (event: incommingEventListInterface, index: number) => (
-              <div key={index} className="w-1/2 px-10  text-white mt-5">
-                <div className="flex flex-wrap  ">
-                  <Link href={event.linkTo}>
-                    <Image src={event.img} alt="" width={291} height={194} />
-                  </Link>
-                  <div className="flex flex-col ml-5 w-1/2">
-                    <h1 className="text-[20px]">{event.eventName}</h1>
-                    <p className="text-[15px]">{event.time}</p>
-
-                    <p>{event.location}</p>
-                  </div>
-                </div>
-              </div>
+              <>
+                <Col span={12}>
+                  <Row>
+                    <Col>
+                      <Link href={event.linkTo}>
+                        <Image
+                          src={event.img}
+                          alt=""
+                          width={300}
+                          height={200}
+                        />
+                      </Link>
+                    </Col>
+                    <Col
+                      className="mx-3 text-ellipsis overflow-hidden"
+                      style={{ width: 250 }}
+                    >
+                      <Row gutter={[24, 24]}>
+                        <Col className="text-xl">{event.eventName}</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col>{event.time}</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col className="mt-5">{event.location}</Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Col>
+              </>
             )
           )}
-        </div>
+        </Row>
+      </Row>
+      <Row justify={"center"}>
+        <Col span={22}>
+          <Divider className=" h-4  border-white" />
+        </Col>
+      </Row>
 
-        <Divider className="my-4 h-4  border-white" variant="middle" />
-
-        <h1 className="text-xl">Complete</h1>
-        <div className="flex flex-col md:flex-row flex-wrap">
+      <Row className="mb-10 text-xl" justify={"center"}>
+        Completed Events
+      </Row>
+      <Row gutter={[24, 24]} justify={"center"}>
+        <Row gutter={[24, 24]} justify={"start"}>
           {completeEvent.map(
             (event: completeEventListInterface, index: number) => (
-              <div key={index} className="w-1/2 px-10 text-white mt-5">
-                <div className="flex flex-wrap">
-                  <Link href={event.linkTo}>
-                    <Image src={event.img} alt="" width={291} height={194} />
-                  </Link>
-                  <div className="flex flex-col ml-5 w-1/2">
-                    <h1 className="text-[20px]">{event.eventName}</h1>
-                    <p className="text-[15px]">{event.time}</p>
-
-                    <p>{event.location}</p>
-                  </div>
-                </div>
-              </div>
+              <>
+                <Col span={12}>
+                  <Row>
+                    <Col>
+                      <Link href={event.linkTo}>
+                        <Image
+                          src={event.img}
+                          alt=""
+                          width={300}
+                          height={200}
+                        />
+                      </Link>
+                    </Col>
+                    <Col
+                      className="mx-3 text-ellipsis overflow-hidden"
+                      style={{ width: 300 }}
+                    >
+                      <Row gutter={[24, 24]}>
+                        <Col className="text-xl">{event.eventName}</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col>{event.time}</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col className="mt-5">{event.location}</Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Col>
+              </>
             )
           )}
-        </div>
-      </div>
+        </Row>
+      </Row>
     </div>
   );
 }
--- app/SingleProject/page.tsx
+++ app/SingleProject/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import StickyNote2Icon from "@mui/icons-material/StickyNote2";
-import MailIcon from "@mui/icons-material/Mail";
 import { Button, Col, Row, Divider } from "antd";
-import Stack from "@mui/material/Stack";
 import { useSession } from "next-auth/react";
 import Comment from "@/components/Comment";
+import { ContainerOutlined } from "@ant-design/icons";
 
 interface projectInterface {
   img: string;
@@ -18,14 +15,9 @@ interface projectInterface {
   contact: string;
 }
 
-interface Comment {
-  id: string;
-  body: string;
-}
-
 const projectArr: projectInterface[] = [
   {
-    img: "/singleImageTest.svg",
+    img: "/Media2.svg",
     description:
       "ISEKAI คือ นิทรรศการเดี่ยวครั้งแรกของ Guong (กวง) - ณภพ เชาวนพูนผล บอกเล่าเกี่ยวกับ สาเหตุแห่งความนิยมมากล้น ที่มีต่อการ์ตูนแนวต่าง",
     projectName: '"ISEKAI"',
@@ -49,37 +41,14 @@ export default function SingleEvent() {
                 <Image
                   src={event.img}
                   alt="Event Pic"
-                  width={1062}
+                  width={562}
                   height={316}
                 />
                 <Row justify="start" className="mt-5 mb-3">
-                  <Col className="xl:text-[20px]">Profile Color</Col>
+                  <Col className="xl:text-[20px]">{event.projectName}</Col>
                 </Row>
                 <Row justify="start" className="mt-5 mb-3">
-                  <Col className="xl:text-[20px]">Project Name</Col>
-                </Row>
-                <Row>
-                  <Col offset={1}>{event.projectName}</Col>
-                </Row>
-                <Row justify="start" className="mt-5 mb-3">
-                  <Col className="xl:text-[20px]">Project Details</Col>
-                </Row>
-                <Row className="mt-5 mb-3">
-                  <Col offset={1}>
-                    <span className="">{event.description}</span>
-                  </Col>
-                </Row>
-                <Row justify="start" className="mt-5 mb-3">
-                  <Col className="xl:text-[20px]">Presentation Files</Col>
-                </Row>
-                <Row>
-                  <Col offset={1}></Col>
-                </Row>
-                <Row justify="start" className="mt-5 mb-3">
-                  <Col className="xl:text-[20px]">Media</Col>
-                </Row>
-                <Row>
-                  <Col offset={1}></Col>
+                  <ContainerOutlined />
                 </Row>
 
                 <Divider className=" h-4  border-white" />
