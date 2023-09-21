0fb10cd278f662278a1f51ef9c5656410ae4793b
immarchz <marcharissawat@gmail.com>
2023-09-21 16:50:13 +0700

    Form

--- app/createForm/page.tsx
+++ app/createForm/page.tsx
@@ -1,238 +1,297 @@
+"use client";
+import {
+  Button,
+  Card,
+  Col,
+  Divider,
+  Form,
+  Input,
+  Row,
+  Select,
+  Upload,
+} from "antd";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
+import { PlusOutlined, SearchOutlined, SyncOutlined } from "@ant-design/icons";
+import { useSession } from "next-auth/react";
+
+const { TextArea } = Input;
+
+interface membersInterface {
+  pic: string;
+  name: string;
+}
+
+const membersArr: membersInterface[] = [
+  {
+    pic: "",
+    name: "Test",
+  },
+];
+
+type FieldType = {
+  title?: string;
+  desc?: string;
+  photo?: string;
+  attach?: string;
+  org?: string;
+  members?: string;
+  link?: string;
+};
 
 export default function createForm() {
-  return (
-    <div className="flex min-h-screen bg-cover bg-black text-white">
-      <div className="flex w-full justify-center px-10">
-        <div className="flex flex-col w-full items-center text-white">
-          <div className="flex flex-col w-2/3 text-2xl">
-            <h1 className="mt-5">About Project</h1>
-          </div>
+  const members = membersArr;
+  const { data: session } = useSession();
 
-          <div className="w-full flex justify-center mt-5">
-            <Box
-              sx={{
-                display: "flex",
-                p: 2,
-                border: "1px solid white",
-                width: 584,
-                height: 248,
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Button className="text-white ">Add a picture</Button>
-            </Box>
-          </div>
-          <div className="flex flex-col w-2/3 text-2xl">
-            <h1 className="mt-5">Project Name</h1>
-            <div className="flex mt-5">
-              <Box
-                component="form"
-                sx={{
-                  width: 1110,
-                  height: 60,
-                  border: "1px solid white",
-                  borderRadius: 5,
-                  display: "flex",
-                  alignItems: "center",
+  if (session && session?.user) {
+    return (
+      <div className="mx-10">
+        <Form name="projectCreate" autoComplete="off">
+          <Row justify={"center"} gutter={[24, 24]} className="">
+            <Col xs={24} xl={8}>
+              <Card
+                className="text-white"
+                style={{
+                  width: "100%",
+                  height: 1051,
+                  backgroundColor: "#1E1E1E",
+                  border: "none",
                 }}
-                autoComplete="off"
               >
-                <TextField
-                  className="flex-grow"
-                  InputProps={{
-                    sx: {
-                      width: 1000,
-                      color: "white",
-                      "& fieldset": {
-                        border: "none",
-                      },
-                    },
-                  }}
-                /> 
-                <Button 
-                className="mr-4 text-black bg-white w-32 hover:bg-white"
-                sx={{
-                    borderRadius:3
+                <Row justify={"start"}>
+                  <Col xs={24} xl={24}>
+                    <div className="mx-8 my-5 ">
+                      <Row className="my-4" justify={"start"}>
+                        <Col span={12}>Title</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col span={24}>
+                          <Form.Item<FieldType>
+                            name="title"
+                            rules={[
+                              {
+                                required: true,
+                                message: "Please input your Project Title!",
+                              },
+                            ]}
+                          >
+                            <Input
+                              style={{
+                                width: "100%",
+                                height: 32,
+                                backgroundColor: "#1E1E1E",
+                                borderRadius: "6px",
+                              }}
+                            />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                      <Row className="mb-4" justify={"start"}>
+                        <Col span={12}>Description</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col span={24}>
+                          <Form.Item<FieldType>
+                            name="desc"
+                            rules={[
+                              {
+                                required: true,
+                                message:
+                                  "Please input your Project Description!",
+                              },
+                            ]}
+                          >
+                            <TextArea
+                              style={{
+                                width: "100%",
+                                height: 106,
+                                backgroundColor: "#1E1E1E",
+                                borderRadius: "6px",
+                                verticalAlign: "top",
+                                textAlign: "left",
+                              }}
+                            />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                      <Row className="mb-4" justify={"start"}>
+                        <Col span={12}>Upload Photo</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col span={24}>
+                          <Form.Item<FieldType> name="photo">
+                            <Upload listType="picture-card">
+                              <PlusOutlined
+                                style={{
+                                  width: "100%",
+                                  color: "white",
+                                }}
+                              />
+                            </Upload>
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                      <Row className="mb-4" justify={"start"}>
+                        <Col span={12}>Attachment</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col span={24}>
+                          <Form.Item<FieldType>
+                            name="attach"
+                            rules={[
+                              {
+                                required: true,
+                                message:
+                                  "Please input your Project Attachment!",
+                              },
+                            ]}
+                          >
+                            <Input
+                              style={{
+                                width: "100%",
+                                height: 32,
+                                backgroundColor: "#1E1E1E",
+                                borderRadius: "6px",
+                              }}
+                            />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+            <Col xs={24} xl={8}>
+              <Card
+                className="text-white "
+                style={{
+                  width: "100%",
+                  height: 1051,
+                  backgroundColor: "#1E1E1E",
+                  border: "none",
                 }}
-                >
-                Edit
-                </Button>
-              </Box>
-            </div>
-
-            <h1 className="mt-5">Project Detail</h1>
-            <div className="flex mt-5">
-              <Box
-                component="form"
-                sx={{
-                  width: 1110,
-                  height: 106,
-                  border: "1px solid white",
-                  borderRadius: 5,
-                  display: "flex",
-                  alignItems: "center",
-                }}
-                autoComplete="off"
               >
-                <TextField
-                  className="flex-grow"
-                  InputProps={{
-                    sx: {
-                      width: 1000,
-                      color: "white",
-                      "& fieldset": {
-                        border: "none",
-                      },
-                    },
-                  }}
-                /> 
-                <Button 
-                className="mr-4 text-black bg-white w-32 hover:bg-white"
-                sx={{
-                    borderRadius:3
-                }}
-                >Edit
-                </Button>
-              </Box>
-            </div>
+                <Row justify={"start"}>
+                  <Col xs={24} xl={24}>
+                    <div className="mx-8 my-5 ">
+                      <Row className="my-4" justify={"start"}>
+                        <Col span={12}>Organizer</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col span={24}>
+                          <Form.Item<FieldType> name="org">
+                            <Input
+                              style={{
+                                width: "100%",
+                                height: 30,
+                                backgroundColor: "#1E1E1E",
+                                borderRadius: "6px",
+                              }}
+                            />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                      <Row className="mb-4" justify={"start"}>
+                        <Col span={12}>Members</Col>
+                      </Row>
+                      <Row className="mb-4" justify={"start"}>
+                        <Col span={12}>Invitation Code</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col span={20}>
+                          <Form.Item<FieldType> name="link">
+                            <Input
+                              addonAfter={
+                                <Button
+                                  type="primary"
+                                  style={{ backgroundColor: "#1E1E1E" }}
+                                >
+                                  Copy
+                                </Button>
+                              }
+                              style={{
+                                width: "100%",
+                                height: 30,
+                                backgroundColor: "#1E1E1E",
 
-            <h1 className="mt-5">Members</h1>
-            <div className="flex mt-5">
-              <Box
-                component="form"
-                sx={{
-                  width: 1110,
-                  height: 106,
-                  border: "1px solid white",
-                  borderRadius: 5,
-                  display: "flex",
-                  alignItems: "center",
-                }}
-                autoComplete="off"
-              >
-                <TextField
-                  className="flex-grow"
-                  InputProps={{
-                    sx: {
-                      width: 1000,
-                      color: "white",
-                      "& fieldset": {
-                        border: "none",
-                      },
-                    },
-                  }}
-                /> 
-                <Button 
-                className="mr-4 text-black bg-white w-32 hover:bg-white"
-                sx={{
-                    borderRadius:3
-                }}
-                >
-                Edit
-                </Button>
-              </Box>
-            </div>
-
-            <h1 className="mt-5">Presentation File(s)</h1>
-            <div className="flex mt-5">
-              <Box
-                component="form"
-                sx={{
-                  width: 1110,
-                  height: 106,
-                  border: "1px solid white",
-                  borderRadius: 5,
-                  display: "flex",
-                  alignItems: "center",
-                }}
-                autoComplete="off"
-              >
-                
-                <TextField
-                disabled
-                  className="flex-grow"
-                  defaultValue="No Files Uploaded"
-                  InputProps={{
-                    sx: {
-                    
-                      width: 1000,
-                      color: "white",
-                      "& fieldset": {
-                        border: "none",
-                      },
-                    },
-                  }}
-                /> 
-                <Button 
-                className="mr-4 text-black bg-white w-32 hover:bg-white"
-                sx={{
-                    borderRadius:3
-                }}
-                >
-                Upload
-                </Button>
-              </Box>
-            </div>
-
-            <h1 className="mt-5">Media(s)</h1>
-            <div className="flex mt-5">
-              <Box
-                component="form"
-                sx={{
-                  width: 1110,
-                  height: 106,
-                  border: "1px solid white",
-                  borderRadius: 5,
-                  display: "flex",
-                  alignItems: "center",
-                }}
-                autoComplete="off"
-              >
-                
-                <TextField
-                  className="flex-grow"
-                  defaultValue="https://www.youtube.com/watch?v=vyqcKf0ilNY"
-                  InputProps={{
-                    sx: {
-                    
-                      width: 1000,
-                      color: "white",
-                      "& fieldset": {
-                        border: "none",
-                      },
-                    },
-                  }}
-                /> 
-                <Button 
-                className="mr-4 text-black bg-white w-32 hover:bg-white"
-                sx={{
-                    borderRadius:3
-                }}
-                >
-                Upload
-                </Button>
-              </Box>
-            </div>
-            
-            
-
-          </div>
-          <Button className="my-12 text-black bg-white w-32 hover:bg-white"
-          sx={{
-            borderRadius:3
-        }}
-          > 
-          Save 
-          </Button>
-        </div>
-        
+                                borderRadius: "6px",
+                              }}
+                            />
+                          </Form.Item>
+                        </Col>
+                        {/* <Col span={4}>
+                          <Button
+                            style={{
+                              color: "white",
+                              width: "100%",
+                              display: "flex",
+                              alignItems: "center",
+                            }}
+                          >
+                            <SyncOutlined />
+                          </Button>
+                        </Col> */}
+                      </Row>
+                      <Row className="mb-4" justify={"start"}>
+                        <Col span={12}>Members</Col>
+                      </Row>
+                      <Row gutter={[24, 24]}>
+                        <Col span={24}>
+                          <Form.Item<FieldType> name="attach">
+                            <Input
+                              placeholder="Filter by name"
+                              style={{
+                                width: "100%",
+                                height: 32,
+                                backgroundColor: "#1E1E1E",
+                                borderRadius: "6px",
+                              }}
+                            />
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                      {members.map((event: membersInterface, index: number) => (
+                        <>
+                          <Row key={index}>
+                            <Col span={2}>
+                              <Image
+                                src={`${session.user.image}`}
+                                alt=""
+                                width="150"
+                                height="150"
+                                className="flex rounded-full "
+                              />
+                            </Col>
+                            <Col span={15} className="flex items-center ml-3">
+                              {event.name}
+                            </Col>
+                            <Col span={6}>
+                              <Select
+                                style={{
+                                  width: "100%",
+                                  color: "black",
+                                }}
+                              />
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Divider style={{ borderColor: "white" }} />
+                          </Row>
+                        </>
+                      ))}
+                      <Row justify={"center"}>
+                        <Button>Save</Button>
+                      </Row>
+                    </div>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          </Row>
+        </Form>
       </div>
-    </div>
-  );
+    );
+  }
 }
