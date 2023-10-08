c74a6f57c35ff462b2416bdbf04be6e34499cce6
amchicken <yooradet.khamon@gmail.com>
2023-10-08 21:29:06 +0700

    add:docker file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,12 +11,15 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    // async signIn({ account, profile }) {
-    //   if (account?.provider === "google") {
-    //     return profile.email_verified && profile.email.endsWith("@example.com")
-    //   }
-    //   return true // Do different verification for other providers that don't have `email_verified`
-    // },
+    async signIn({ account, profile }) {
+      console.log(profile);
+      return true;
+
+      // if (account?.provider === "google") {
+      //   return profile.email_verified && profile.email.endsWith("@example.com")
+      // }
+      // return true // Do different verification for other providers that don't have `email_verified`
+    },
   },
   providers: [
     GoogleProvider({
