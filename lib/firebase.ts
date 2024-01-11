a25f2205781463e2e1198c1a275fb6920bea3eb3
amchicken <yooradet.khamon@gmail.com>
2024-01-11 21:44:29 +0700

    update:env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcehZ1CuluOySPGLl6LOm-HP6gIvSlDKE",
-  authDomain: "gallery-walk-5a404.firebaseapp.com",
-  projectId: "gallery-walk-5a404",
-  storageBucket: "gallery-walk-5a404.appspot.com",
-  messagingSenderId: "904558761214",
-  appId: "1:904558761214:web:8322dbe455935b6f4301af",
+  apiKey: "AIzaSyCCpuDcqdAJU5iR-T9Wwk5fYDMHRUIRY_U",
+  authDomain: "gallery-walk-b3312.firebaseapp.com",
+  projectId: "gallery-walk-b3312",
+  storageBucket: "gallery-walk-b3312.appspot.com",
+  messagingSenderId: "212046405718",
+  appId: "1:212046405718:web:816da28c19950ae98e9bf7",
 };
 
 const app = initializeApp(firebaseConfig);
